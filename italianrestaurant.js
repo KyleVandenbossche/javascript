b4838c11aa0d35720ecdb1edd24e9959eb238155
Kyle Vandenbossche <kylevandenbossche@gmail.com>
2023-02-05 19:44:59 -0500

    updated italianrestaurant

--- italianrestaurant.js
+++ italianrestaurant.js
@@ -1,3 +1,75 @@
+const prompt = require('prompt-sync')();
+
+const price = 0;
+
+
+askAboutPizza();
+
+function askAboutPizza() {
+    const size = prompt("What size pizza would you like? (small, medium, or large)");
+    let price;
+    
+    switch (size) {
+      case "small":
+        price = 5;
+        break;
+      case "medium":
+        price = 6;
+        break;
+      case "large":
+        price = 7;
+        break;
+        default:
+      return {
+        size: null,
+        price: null
+      };
+
+  }
+}
+
+askAboutSalad();
+
+function askAboutSalad() {
+    const saladsize = prompt("What size salad would you like? (small or large)");
+    let price;
+    
+    switch (saladsize) {
+      case "small":
+        price = 2;
+        break;
+      case "large":
+        price = 4;
+        break;
+        default:
+      return {
+        size: null,
+        price: null
+      };
+  }
+}
+
+askAboutDelivery();
+
+function askAboutDelivery() {
+    const size = prompt("Would you like delivery? (yes or no))");
+    let price;
+    
+
+    if (size === 'yes'){
+        const address = prompt("What is your address?")
+        price = price+10
+        console.log(price);
+
+    } else {
+        return;
+    };
+
+    }
+
+ 
+    
+  
 
 
 
@@ -6,6 +78,19 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function askAboutPizza(size, price){
     const answer = {
         Small: 5,
@@ -20,3 +105,4 @@ function askAboutSalad(){
         large: 4,
     }
 }
+*/
