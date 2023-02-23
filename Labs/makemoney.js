a1459732e5d3ffdf1f288dfb71323914f1456cc6
Kyle Vandenbossche <kylevandenbossche@gmail.com>
2023-02-22 19:17:54 -0500

    almost final

--- Labs/makemoney.js
+++ Labs/makemoney.js
@@ -1,42 +1,26 @@
 (() => {
-    console.log('hello')
 
+   
+    
+    const makeMoneyButton = document.getElementById('makeMoneyButton');
+ 
+
+    makeMoneyButton.addEventListener("click", event => {
+    event.preventDefault();
     const howManyInput = document.getElementById('number');
     const coinType = document.getElementById('coinType');
-    const makeMoneyButton = document.getElementById('makeMoneyButton');
-    const coinContainer = document.getElementById('coin-container');
-    // console.log(howManyInput);
-    // console.log(coinType);
-    // console.log(makeMoneyButton);
-
-    // function makeMoney(event) {
-    //     event.preventDefault();
-    //    const howManyInputValue = +howManyInput.value;
-    //    for (let i = 0; i<howManyInputValue; i++){
-    //     const newCoin= document.createElement('div');
-    //     const newCoinP = document.createElement('p');
-    //     newCoinP.innerText.coinType.value;
-    //     newCoin.append(newCoinP);
-    //     newCoin.classList.add('coin');
-    //     coinContainer.append(newCoin);
-    //    }
-        
-
-    // }
-
-function makeMoney(event){
-    event.preventDefault();
     const howManyInputValue = +howManyInput.value;
+
     for (let i = 0; i<howManyInputValue; i++){
         const newCoin = document.createElement("div");
         // newCoin.innerText = document.getElementById("coinType");
-        coinContainer.appendChild(newCoin);
+
+        newCoin.classList.add(coinType.value);
+        document.body.append(newCoin);
     }
-}
+})}
 
 
 
-makeMoneyButton.addEventListener('click', makeMoney);
 
-
-} )();
+)();
